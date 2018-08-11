d4fb4c2ee5ef485991fb7359b60d5d6a1131414c
Alaa Shabouni <alaashabouni@gmail.com>
2018-08-10 17:35:17 -0700

    player keyboard movements

--- js/app.js
+++ js/app.js
@@ -32,20 +32,45 @@ Enemy.prototype.render = function() {
 
 //Hero class
 class Hero {
-  constructor(){
-    this.x = 0;
-    this.y = 0;
+  constructor() {
+    this.step = 101;
+    this.jump = 83;
+    this.startX = this.step * 2;
+    this.startY = this.jump * 5;
+    this.x = this.startX;
+    this.y = this.startY;
     this.sprite = 'images/char-boy.png';
   }
   render (){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
+
+    handleInput(input) {
+      switch(input) {
+        case 'left':
+          if (this.x >0) {
+              this.x -= this.step;
+            }
+          break;
+        case 'up':
+          if (this.y > this.jump) {
+              this.y -= this.jump;
+            }
+          break;
+        case 'right':
+          if (this.x < this.step * 4) {
+              this.x += this.step;
+            }
+          break;
+        case 'down':
+          if (this.y < this.jump * 5){
+              this.y += this.jump;
+            }
+          break;
+    }
 }
-  //Contstruction
-    //properties
-        //x position
-        //y position
-        //sprite images
+}
+
     //methods
       //update position
         //check collision
@@ -63,6 +88,7 @@ const player = new Hero();
 
 //Init allEnemies array
 //For each enemy create and push new enemy object into above array
+
 
 
 
