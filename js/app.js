beb4b638cd812ecf2e02b32ec14fb1e482ea484e
Alaa Shabouni <alaashabouni@gmail.com>
2018-08-13 15:05:06 -0700

    enemy + hero collision detection

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    this.x = 0;
-    this.y = 55;
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y + 55;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
     this.step = 101;
     this.boundary = this.step * 5;
@@ -19,7 +20,7 @@ Enemy.prototype.update = function(dt) {
     if(this.x < this.boundary) {
       //move forward
       //Increment x by speed * dt
-      this.x += 200 * dt;
+      this.x += this.speed * dt;
     }
 
     else {
@@ -38,7 +39,7 @@ class Hero {
     this.step = 101;
     this.jump = 83;
     this.startX = this.step * 2;
-    this.startY = this.jump * 5;
+    this.startY = (this.jump * 4) + 55;
     this.x = this.startX;
     this.y = this.startY;
     this.sprite = 'images/char-boy.png';
@@ -69,16 +70,25 @@ class Hero {
               this.y += this.jump;
             }
           break;
+        }
     }
-}
+  update() {
+    //check collision
+    for(let enemy of allEnemies){
+      //Did player colide with enemy?
+      if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x
+      && enemy.x < this.x + this.step/2)) {
+          this.reset();
+      }
+    }
+    //check win
+      //Did player positon win game?
+  }
 }
 
     //methods
       //update position
-        //check collision
-          //Did player colide with enemy?
-        //check win
-          //Did player positon win game?
+
       //render
           //Draw player sprite on x and y coord
       //Handle keyboard input
@@ -87,12 +97,12 @@ class Hero {
 
 //New Hero object - in a variable called player
 const player = new Hero();
-const bug1 = new Enemy();
-
-//Init allEnemies array
-//For each enemy create and push new enemy object into above array
+const bug1 = new Enemy(-101, 0, 200);
+const bug2 = new Enemy(-101, 83, 300);
+const bug3 = new Enemy((-101*2.5), 83, 300);
 const allEnemies = [];
-allEnemies.push(bug1);
+allEnemies.push(bug1, bug2, bug3);
+console.log(allEnemies);
 
 
 // This listens for key presses and sends the keys to your
